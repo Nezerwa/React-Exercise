04530e396391dce23a94de7ba7b2f50b91a6a551
Nezerwa <eligrand2000@gmail.com>
2023-10-01 15:36:17 +0200

    Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ function App() {
     alert('Clicked!')
   }
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-10 bg-yellow-500 w-[500px]">
       <h1 className="text-3xl">Event handling exercise</h1>
-      <button className=" bg-blue-700 text-xl text-white rounded w-40" onClick={clickHandler}>Click Me</button>
+      <button className=" bg-gray-400 text-xl text-white rounded w-40" onClick={clickHandler}>Click Me</button>
     </div>
   );
 }

60f1d38b16160ae65bc9d4abcb9331dd783da3a5
Nezerwa <eligrand2000@gmail.com>
2023-10-01 16:06:51 +0200

    Revert commit that have a mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 function App() {
-  const clickHandler = ()=>{
-    alert('Clicked!')
-  }
-  return (
-    <div className="flex flex-col gap-10">
-      <h1 className="text-3xl">Event handling exercise</h1>
-      <button className=" bg-blue-700 text-xl text-white rounded w-40" onClick={clickHandler}>Click Me</button>
-    </div>
-  );
+ return (
+  <div>React exercises</div>
+ )
 }
 
 export default App;

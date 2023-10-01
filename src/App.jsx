542b47d16c29189f7ad430fc5063ece6531b119f
Nezerwa <eligrand2000@gmail.com>
2023-10-01 14:02:53 +0200

    Add hello word

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div>
+    <div className="bg-yellow-500 h-[500px] w-[500px] flex items-center justify-center mx-auto my-32">
       <p>Hello, World!</p>
     </div>
   );

5f7a139e883ddcd0999ea6feb00faeac07e6e7af
Nezerwa <eligrand2000@gmail.com>
2023-10-01 17:50:27 +0200

    use declarative approach to render list items from array

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
- return (
-  <div>React exercises</div>
- )
+  const arr = ["dog", "cat", "chicken", "cow", "sheep", "horse"].map(
+    (elements) => <li className="text-xl">{elements}</li>
+  );
+  return (
+    <div className="flex flex-col my-36 px-32">
+      <ul className="list-disc">{arr}</ul>
+    </div>
+  );
 }
 
 export default App;

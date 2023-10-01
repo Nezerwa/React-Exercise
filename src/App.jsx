d2dff081add688ed53a7294c46e3e570fd961b93
Nezerwa <eligrand2000@gmail.com>
2023-10-01 16:34:34 +0200

    Create Button custom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 function App() {
-  const clickHandler = ()=>{
-    alert('Clicked!')
-  }
-  return (
-    <div className="flex items-center justify-center h-[500px]">
-      <button className=" bg-gray-400 text-xl text-white rounded w-40" onClick={clickHandler}>Click Me</button>
-    </div>
-  );
+ 
 }
 
 export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,17 @@
+function ButtonComponent(){
+     const clickHandler = () => {
+       alert("Clicked!");
+     };
+     return (
+       <div className="flex items-center justify-center h-[500px]">
+         <button
+           className=" bg-gray-400 text-xl text-white rounded w-40"
+           onClick={clickHandler}
+         >
+           Click Me
+         </button>
+       </div>
+     );
+}
+
+export default ButtonComponent

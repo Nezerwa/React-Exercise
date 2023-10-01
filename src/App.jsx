8c99ce2862a24acce6d1e14bce82f01f406591f1
Nezerwa <eligrand2000@gmail.com>
2023-10-01 14:31:55 +0200

    Event handling exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <h1 className="text-red-700">Hello react</h1>;
+  const clickHandler = ()=>{
+    alert('Clicked!')
+  }
+  return (
+    <div className="flex flex-col gap-10">
+      <h1 className="text-3xl">Event handling exercise</h1>
+      <button className=" bg-blue-700 text-xl text-white rounded w-40" onClick={clickHandler}>Click Me</button>
+    </div>
+  );
 }
 
 export default App;

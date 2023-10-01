c4b269aa25182e565a050c871598c4abff3dcc2a
Nezerwa <eligrand2000@gmail.com>
2023-10-01 17:23:39 +0200

    Create a state which is responsible for re rendering onclick

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 function ButtonComponent() {
+  let [count, setCount] = useState(0);
+  const onclickHandler = () => {
+    setCount((prev) => prev + 1);
+  };
   return (
     <div className="relative top-24 left-80">
-      <p className="text-lg">button has clicked times</p>
-      <button className="text-lg bg-slate-400 px-5 mt-6">Click me</button>
+      <p className="text-lg">button has clicked: {count} times</p>
+      <button className="text-lg bg-slate-400 px-5 mt-6" onClick={onclickHandler}>Click me</button>
     </div>
   );
 }
 
-export default ButtonComponent
+export default ButtonComponent;

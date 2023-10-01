f1bda3bcbf9d3c38f2b56d0a05c4111dd7ac5c46
Nezerwa <eligrand2000@gmail.com>
2023-10-01 17:18:49 +0200

    Add a button component

---
+++ src/components/button.jsx
@@ -0,0 +1,10 @@
+function ButtonComponent() {
+  return (
+    <div className="relative top-24 left-80">
+      <p className="text-lg">button has clicked times</p>
+      <button className="text-lg bg-slate-400 px-5 mt-6">Click me</button>
+    </div>
+  );
+}
+
+export default ButtonComponent

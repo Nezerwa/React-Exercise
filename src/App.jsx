a05e3bf89a6bd33dbd62a40116a97abea6e14715
Nezerwa <eligrand2000@gmail.com>
2023-10-01 15:51:55 +0200

    Add event hundler

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     alert('Clicked!')
   }
   return (
-    <div className="flex items-center justify-center h-[500px]">
+    <div className="flex items-center justify-center h-[600px]">
       <button className=" bg-gray-400 text-xl text-white rounded w-40" onClick={clickHandler}>Click Me</button>
     </div>
   );

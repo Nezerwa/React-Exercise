2faff51837e60f73fffd08a4ba678792c52a1ccb
Nezerwa <eligrand2000@gmail.com>
2023-10-01 16:59:01 +0200

    use get children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import ButtonComponent from "./components/button";
 function App() {
   return (
-    <div>
-      <ButtonComponent />
+    <div className="flex gap-5 items-center h-60 justify-center">
+      <ButtonComponent>1</ButtonComponent>
+      <ButtonComponent>2</ButtonComponent>
+      <ButtonComponent>3</ButtonComponent>
+      <ButtonComponent>4 </ButtonComponent>
     </div>
   );
 }
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,17 +1,17 @@
-function ButtonComponent(){
-     const clickHandler = () => {
-       alert("Clicked!");
-     };
-     return (
-       <div className="flex items-center justify-center h-[500px]">
-         <button
-           className=" bg-gray-400 text-xl text-white rounded w-40"
-           onClick={clickHandler}
-         >
-           Click Me
-         </button>
-       </div>
-     );
+function ButtonComponent(props) {
+  const clickHandler = () => {
+    alert(`You have clicked on ${props.children}`);
+  };
+  return (
+    <div>
+      <button
+        className=" bg-gray-400 text-xl text-white rounded w-40"
+        onClick={clickHandler}
+      >
+        Button {props.children}
+      </button>
+    </div>
+  );
 }
 
-export default ButtonComponent
+export default ButtonComponent;

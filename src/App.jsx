f9a906d02ab11fa2bf54847112c627bdab5a11e7
Nezerwa <eligrand2000@gmail.com>
2023-10-02 16:37:44 +0200

    add functionality to button

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Flip from "./components/Flip";
 
 function App() {
   const [data, setData] = useState([]);
+  const [change, setChange] = useState(true);
   useEffect(() => {
     fetch("https://random-data-api.com/api/users/random_user?size=10")
       .then((res) => res.json())
       .then((res) => setData(res));
-  }, []);
-  const cards = data.map((items) => {
-    return <Flip {...items} key={items.id} />;
-  });
+  }, [change]);
+   const handleClick = () => setChange((prev) => !prev);
+
+  const cards = data.map((items) => <Flip {...items} key={items.id} />);
+
   return (
     <div className="bg-white">
       <div className="flex justify-center pb-10 pt-10">
-        <button className="bg-red-600 py-2 px-12 text-white">
+        <button className="bg-red-600 py-2 px-12 text-white" onClick={handleClick}>
           Fetch Random
         </button>
       </div>

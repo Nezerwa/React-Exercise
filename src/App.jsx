ff8a06e6205b9a0df9dc7470e7959bb5a7a5c78a
Nezerwa <eligrand2000@gmail.com>
2023-11-13 10:49:34 +0200

    add add and remove functionality to my todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,77 @@
-import { useEffect, useState } from "react";
-import Flip from "./components/Flip";
+import { useState } from "react";
+import { FaPlusCircle, FaTrash } from "react-icons/fa";
+import { connect } from "react-redux";
+import { addTodos, removeTodos } from "./redux/reducer";
 
-function App() {
-  const [data, setData] = useState([]);
-  const [change, setChange] = useState(true);
-  useEffect(() => {
-    fetch("https://random-data-api.com/api/users/random_user?size=10")
-      .then((res) => res.json())
-      .then((res) => setData(res));
-  }, [change]);
-   const handleClick = () => setChange((prev) => !prev);
+const myStateProps = (state) => {
+  return {
+    todos: state,
+  };
+};
 
-  const cards = data.map((items) => <Flip {...items} key={items.id} />);
-
+const dispatchProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+    removeTodo: (id) => dispatch(removeTodos(id)),
+  };
+};
+function App(props) {
+  const [Todo, SetTodo] = useState("")
+  const handleChange = (e) => {
+    SetTodo(e.target.value);
+  };
   return (
-    <div className="bg-white">
-      <div className="flex justify-center pb-10 pt-10">
-        <button className="bg-red-600 py-2 px-12 text-white" onClick={handleClick}>
-          Fetch Random
-        </button>
-      </div>
-      <div className="w-[80%]">
-        <div className="grid grid-cols-2 w-full  gap-y-12 py-12 pl-10 gap-x-24 mx-32">
-          {cards}
-        </div>
-      </div>
+    <div className="bg-white flex w-full h-screen flex-col">
+      <header className="flex flex-col gap-4">
+        <h1 className="p-5 text-[90px] text-center text-gray-400">todolist</h1>
+        <form
+          action=""
+          className="rounded-lg mx-auto w-4/6  flex px-5 py-2 shadow-lg"
+        >
+          <input
+            type="text"
+            placeholder="Enter a Todo"
+            className="focus:outline-none text-2xl flex-1"
+            onChange={handleChange}
+          />
+          <button
+            type="button"
+            className="text-2xl"
+            onClick={() => {
+              if (Todo.trim() !== "") {
+                props.addTodo({
+                  id: Math.floor(Math.random() * 1000),
+                  item: Todo,
+                  completed: false,
+                });
+                SetTodo("");
+              }
+            }}
+          >
+            <FaPlusCircle className="text-green-700" />
+          </button>
+        </form>
+        <ul className="flex gap-2 mx-60 flex-col">
+          {props.todos.map((item) => {
+            return (
+              <li
+                key={item.id}
+                className="mx-5 my-0 flex justify-between w-full border-b-2 mb-2"
+              >
+                <span className="text-lg">{item.item} </span>
+                <button
+                  onClick={() => {
+                    props.removeTodo(item.id);
+                  }}
+                >
+                  <FaTrash className="text-red-600" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </header>
     </div>
   );
 }
-
-export default App;
+export default connect(myStateProps, dispatchProps)(App);
---
+++ src/redux/reducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = [];
+const addTodoReducer = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    //reducers
+    addTodos: (state, action) => {
+      state.push(action.payload);
+      return state;
+    },
+    removeTodos: (state, action) => {
+      return state.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodos, removeTodos } = addTodoReducer.actions;
+export const reducer = addTodoReducer.reducer;
